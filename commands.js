7832453bd3f1dede5632a4bc1019fe3bb05e888d
blobfysh <47480729+blobfysh@users.noreply.github.com>
2019-03-28 02:36:10 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -3281,7 +3281,7 @@ class Commands {
         if(helpCommand !== undefined){
             return methods.commandhelp(message, helpCommand, prefix);
         }
-        let otherCmds = ["`rules`","`cooldown`","`delete`","`deactivate`","`server`","`update`","`health`","`money`","`level`","`points`","`leaderboard [s]`","`setprefix`","`discord`","`profile [@user]`","`upgrade [skill]`"]
+        let otherCmds = ["`rules`","`cooldowns`","`delete`","`deactivate`","`server`","`update`","`health`","`money`","`level`","`points`","`leaderboard [s]`","`setprefix`","`discord`","`profile [@user]`","`upgrade [skill]`","`backpack`"]
         otherCmds.sort();
         const helpInfo = new Discord.RichEmbed()
         .setTitle("`"+prefix+"play`** - Adds you to the game.**")
@@ -4112,6 +4112,7 @@ class Commands {
                 "`" + prefix + "unban <id>` - Unbans user and sends them message stating they've been unbanned.",
                 "`" + prefix + "status <activity> <status>` - Sets bot status.",
                 "`" + prefix + "getbans` - Displays list of all banned users.",
+                "`" + prefix + "getbaninfo <id>` - Shows reason and date for banned user.",
                 "`" + prefix + "invwipe <id> <reason>` - Wipes a users data and sends them message with reason. Will also log the users inventory and unique code prior to wipe in <#500467081226223646>.",
                 "`" + prefix + "getinv <id>` - Displays a users inventory along with their unique inventory code.",
                 "`" + prefix + "restoreinv <unique inventory code>` - Restores a users inventory using a code from either the getinv or invwipe commands."
