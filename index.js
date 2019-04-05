b1061ca11eeecfe27ef116d26db3e033689df35d
blobfysh <47480729+blobfysh@users.noreply.github.com>
2019-04-05 09:06:09 -0400

    added ability to display leaderboard on website
    with the new sendlbtoweb() method

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ global.weapCooldown = new Set();  //weapon cooldown stuff
 var xpNeeded; //is set to players xp needed when they send a message | used to determine level and used in t-inv command to calculate xp left until next level
 var totalXpNeeded = 0;
 
-const version = "3.11.0";
+const version = "3.11.1";
 
 client.on(`ready`,() => {
     console.log(" _                    _                           _ \n"+
