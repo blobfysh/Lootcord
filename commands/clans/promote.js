a10879289999a0811fed8c771d4fbef131d612fe
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-01-25 13:32:11 -0500

    Fix newly created clans not having leaders

--- commands/clans/promote.js
+++ commands/clans/promote.js
@@ -37,7 +37,7 @@ module.exports = {
         else if(message.author.id == invitedUser.id){
             return message.reply(lang.errors[1]);
         }
-        else if((invitedScoreRow.clanRank + 1) >= scoreRow.clanRank){
+        else if(clan_ranks[invitedScoreRow.clanRank + 1].title !== 'Leader' && (invitedScoreRow.clanRank + 1) >= scoreRow.clanRank){
             return message.reply('You cannot promote members to an equal or higher rank!');
         }
         else if(clan_ranks[invitedScoreRow.clanRank + 1].title == 'Leader'){
