c6951220aa25cd1c076a7052e520b51eae2635a7
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-03-07 01:23:30 -0500

    Leaderboard defaults to server
    - global leaderboard now cached
    - add silentban command

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -2,7 +2,8 @@ const Discord   = require('discord.js');
 const { query } = require('../mysql.js');
 const methods   = require('../methods/methods.js');
 const globalLB  = require('../methods/global_leaderboard.js');
-const general = require('../methods/general');
+const general   = require('../methods/general');
+const cache     = require('../utils/cache');
 
 module.exports = {
     name: 'leaderboard',
@@ -15,86 +16,8 @@ module.exports = {
     adminOnly: false,
     
     async execute(message, args, lang, prefix){
-        var leaders      = [];
-        var levelLeaders = [];
-        var killLeaders  = [];
-        var tokenLeaders = [];
-
-        if(args[0] == 's' || args[0] == 'server'){
-            var guildUsers = [];
-
-            function compareValues(key, order='desc') {
-                return function(a, b) {
-                    if(!a.hasOwnProperty(key) || 
-                        !b.hasOwnProperty(key)) {
-                        return 0; 
-                    }
-                
-                    const varA = (typeof a[key] === 'string') ? 
-                    a[key].toUpperCase() : a[key];
-                    const varB = (typeof b[key] === 'string') ? 
-                    b[key].toUpperCase() : b[key];
-                        
-                    let comparison = 0;
-                    if (varA > varB) {
-                        comparison = 1;
-                    } else if (varA < varB) {
-                        comparison = -1;
-                    }
-                    return (
-                        (order == 'desc') ? 
-                        (comparison * -1) : comparison
-                    );
-                };
-            }
-
-            const guildRows = await query(`SELECT * FROM userGuilds
-            INNER JOIN scores 
-            ON userGuilds.userId = scores.userId
-            WHERE userGuilds.guildId ="${message.guild.id}"`);
-
-            guildRows.forEach(row => {
-                var newPlayerObj = {USERID : row.userId, MONEY : row.money, LEVEL : row.level}
-
-                guildUsers.push(newPlayerObj);
-            });
-
-            guildUsers.sort(compareValues('MONEY'));
-
-            for(var i = 0; i < guildUsers.length; i++){
-                try{
-                    leaders.push(`💵**${(await general.getUserInfo(message, guildUsers[i].USERID, true)).user.tag}**` + ' - ' + methods.formatMoney(guildUsers[i].MONEY, true));
-                }
-                catch(err){
-                }
-            }
-
-            guildUsers.sort(compareValues('LEVEL'));
-
-            for(var i = 0; i < guildUsers.length; i++){
-                try{
-                    levelLeaders.push(`🔹**${(await general.getUserInfo(message, guildUsers[i].USERID, true)).user.tag}**` + ' - Level ' + guildUsers[i].LEVEL);
-                }
-                catch(err){
-                }
-            }
-
-            var newMoney = leaders.slice(0,10);
-            var newLevel = levelLeaders.slice(0,10);
-            newMoney[0] = newMoney[0].replace("💵", "💰");
-            newLevel[0] = newLevel[0].replace("🔹", "💠");
-
-            const embedLeader = new Discord.RichEmbed() 
-            .setTitle(`**Server Leaderboard**`)
-            .setThumbnail(message.guild.iconURL)
-            .setColor(13215302)
-            .addField("Money", newMoney)
-            .addField("Level", newLevel)
-            .setFooter("Top " + newLevel.length)
-            message.channel.send(embedLeader);
-        }
-        else{
-            const leaders = await globalLB.create_lb(message.client);
+        if(args[0] == 'g' || args[0] == 'global'){
+            const leaders = await getGlobalLB(message.client);
 
             const embedLeader = new Discord.RichEmbed() 
             .setTitle(`**Global Leaderboard**`)
@@ -106,7 +29,91 @@ module.exports = {
             .setFooter("Top 5")
 
             return message.channel.send(embedLeader);
+        }
+        else{
+            let leaders      = [];
+            let levelLeaders = [];
+            let killLeaders  = [];
+
+            const moneyRows = await query(`SELECT scores.userId, money 
+            FROM userGuilds 
+            INNER JOIN scores 
+            ON userGuilds.userId = scores.userId 
+            WHERE userGuilds.guildId ="${message.guild.id}" 
+            ORDER BY money DESC LIMIT 3`);
+            const levelRows = await query(`SELECT scores.userId, level 
+            FROM userGuilds 
+            INNER JOIN scores 
+            ON userGuilds.userId = scores.userId 
+            WHERE userGuilds.guildId ="${message.guild.id}" 
+            ORDER BY level DESC LIMIT 3`);
+            const killRows  = await query(`SELECT scores.userId, kills 
+            FROM userGuilds 
+            INNER JOIN scores 
+            ON userGuilds.userId = scores.userId 
+            WHERE userGuilds.guildId ="${message.guild.id}" 
+            ORDER BY kills DESC LIMIT 3`);
+
+            for(var key in moneyRows){
+                try{
+                    let userInfo = await general.getUserInfo(message, moneyRows[key].userId, true);
+                    leaders.push(`💵 **${userInfo.user.tag}**` + ' - ' + methods.formatMoney(moneyRows[key].money, true));
+                }
+                catch(err){
+                    console.log(err);
+                }
+            }
+            for(var key in levelRows){
+                try{
+                    let userInfo = await general.getUserInfo(message, levelRows[key].userId, true);
+                    levelLeaders.push(`🔹 **${userInfo.user.tag}**` + ' - Level  ' + levelRows[key].level);
+                }
+                catch(err){
+        
+                }
+            }
+            for(var key in killRows){
+                try{
+                    let userInfo = await general.getUserInfo(message, killRows[key].userId, true);
+                    killLeaders.push(`🏅 **${userInfo.user.tag}**` + ' - ' + killRows[key].kills + " kills");
+                }
+                catch(err){
+                    
+                }
+            }
             
+            leaders[0] = leaders[0].replace("💵", "💰");
+            levelLeaders[0] = levelLeaders[0].replace("🔹","💠");
+            killLeaders[0] = killLeaders[0].replace("🏅","🏆");
+
+            const embedLeader = new Discord.RichEmbed() 
+            .setTitle(`**Server Leaderboard**`)
+            .setThumbnail(message.guild.iconURL)
+            .setColor(13215302)
+            .addField("Money", leaders)
+            .addField("Level", levelLeaders)
+            .addField("Kills", killLeaders)
+            .setFooter("Top " + leaders.length)
+            message.channel.send(embedLeader);
         }
     },
+}
+
+async function getGlobalLB(client){
+    cacheLB = cache.get('leaderboard');
+
+    if(!cacheLB){
+        try{
+            const leaders = await globalLB.create_lb(client);
+            
+            cache.set('leaderboard', leaders);
+            return leaders;
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    else{
+        return cacheLB;
+    }
 }
