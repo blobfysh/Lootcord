72e8f59632ea0a7224b2976a89ed819b21ad329a
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-05-03 08:06:03 -0400

    add alias for item command

--- src/commands/info/items.js
+++ src/commands/info/items.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     name: 'items',
-    aliases: ['item'],
+    aliases: ['item', 'recipe'],
     description: 'Shows information about an item.',
     long: 'Specify an item to see detailed information about it.',
     args: {"item": "Item to search."},
