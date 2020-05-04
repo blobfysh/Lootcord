17751d8a870afdfd0f5a5023735de48a807145b2
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-05-04 01:20:39 -0400

    Report command now generalized
    - users can send any type of report content

--- src/commands/other/report.js
+++ src/commands/other/report.js
@@ -3,71 +3,62 @@ module.exports = {
     name: 'report',
     aliases: [''],
     description: "Report another player for breaking the rules.",
-    long: "Report another player to the mods. Supports image attachments.",
-    args: {"discord#tag": "The tag of the user you're reporting or their user ID.", "reason": "The reason for reporting them."},
-    examples: ["report blobfysh#4679 Looked at me funny"],
+    long: "Send a report to the moderators. Examples include cheaters, alt accounts or just general errors you have with the bot. Supports image attachments.",
+    args: {"message": "The content of your report."},
+    examples: ["report blobfysh#4679 looked at me funny"],
     ignoreHelp: false,
     requiresAcc: true,
     requiresActive: false,
     guildModsOnly: false,
     
     async execute(app, message){
-        let userOldID = message.args[0];
-        let messageIn = message.args.slice(1).join(" ");
+        let messageIn = message.args.join(" ");
         let reportCD = await app.cd.getCD(message.author.id, 'report');
 
         if(reportCD){
-            message.reply(`You just sent a report! Wait \`${reportCD}\` before sending another.`);
-        }
-        else if(userOldID == undefined){
-            message.reply('You need to include a discordtag#1234 and the reason for reporting! `' + message.prefix + 'report <discord#tag> <reason for report>`');
+            return message.reply(`You just sent a report! Wait \`${reportCD}\` before sending another.`);
         }
         else if(messageIn == ""){
-            message.reply("You forgot to put a message! `" + message.prefix + "report <discordtag#1234> <reason for report>`");
+            return message.reply("You forgot to put a message! `" + message.prefix + "report <content>`");
         }
-        else if(/^<?@?!?(\d+)>?$/.test(userOldID) || /^(.*)#([0-9]{4})$/.test(userOldID)){
-            const botMessage = await message.reply("Confirm this report? **" + userOldID + "**\n\n**Spamming this command or using it for purposes other than reporting will result in a warning or ban.**");
-            
-            try{
-                let confirmed = await app.react.getConfirmation(message.author.id, botMessage);
 
-                if(confirmed){
-                    let imageAttached = message.attachments;
+        const botMessage = await message.reply("Submit report?\n\n**Spamming this command or using it for purposes other than reporting will result in a warning or ban.**");
 
-                    const reportEmbed = new app.Embed()
-                    .setAuthor("New Report")
-                    .setThumbnail(message.author.avatarURL)
-                    .addField("Submitted by", `${message.author.tag}\n(${message.author.id})`)
-                    .addField("User", userOldID)
-                    .addField("Reason", messageIn)
-                    .setFooter(`Respond with t-message ${message.author.id} <message>`)
-                    .setColor(16734296)
-                    
-                    if(Array.isArray(imageAttached) && imageAttached.length){
-                        if(imageAttached[0].url.endsWith(".mp4") || imageAttached[0].url.endsWith(".mp3")){
-                            reportEmbed.addField("File", imageAttached[0].url)
-                        }
-                        else{
-                            reportEmbed.setImage(imageAttached[0].url);
-                        }
+        try{
+            let confirmed = await app.react.getConfirmation(message.author.id, botMessage);
+
+            if(confirmed){
+                let imageAttached = message.attachments;
+
+                const reportEmbed = new app.Embed()
+                .setAuthor("New Report")
+                .setThumbnail(message.author.avatarURL)
+                .addField("Submitted by", `${message.author.tag}\n(${message.author.id})`)
+                .addField("Message", messageIn)
+                .setFooter(`Respond with t-message ${message.author.id} <message>`)
+                .setColor(16734296)
+                
+                if(Array.isArray(imageAttached) && imageAttached.length){
+                    if(imageAttached[0].url.endsWith(".mp4") || imageAttached[0].url.endsWith(".mp3")){
+                        reportEmbed.addField("File", imageAttached[0].url)
                     }
-
-                    app.messager.messageMods({embed: reportEmbed.embed}, { ping: true });
-
-                    app.cd.setCD(message.author.id, 'report', 300 * 1000);
-
-                    botMessage.edit("Report successfully sent!");
+                    else{
+                        reportEmbed.setImage(imageAttached[0].url);
+                    }
                 }
-                else{
-                    botMessage.delete();
-                }
+
+                app.messager.messageMods({embed: reportEmbed.embed}, { ping: false });
+
+                app.cd.setCD(message.author.id, 'report', 300 * 1000);
+
+                botMessage.edit("Report successfully sent!");
             }
-            catch(err){
-                botMessage.edit("You didn't react in time!");
+            else{
+                botMessage.delete();
             }
         }
-        else{
-            message.reply('You must include a valid discordtag#1234 or user ID.');
+        catch(err){
+            botMessage.edit("You didn't react in time!");
         }
     },
 }
