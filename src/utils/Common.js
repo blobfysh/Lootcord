9f0deb4dae335caab3f702275500b537424f1701
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-06-03 10:08:27 -0400

    new guilds default to selective attacks

--- src/utils/Common.js
+++ src/utils/Common.js
@@ -34,7 +34,7 @@ class Common {
         
         if(!guildInfo){
             console.warn('Creating new guildInfo row');
-            await this.app.query(`INSERT IGNORE INTO guildInfo (guildId, killChan, levelChan, dropChan, dropItemChan, dropItem, randomOnly) VALUES (?, 0, 0, 0, 0, '', 1)`, [guildId]);
+            await this.app.query(`INSERT IGNORE INTO guildInfo (guildId, killChan, levelChan, dropChan, dropItemChan, dropItem, randomOnly) VALUES (?, 0, 0, 0, 0, '', 0)`, [guildId]);
             guildInfo = (await this.app.query(`SELECT * FROM guildInfo WHERE guildId = ?`, [guildId]))[0];
         }
         
