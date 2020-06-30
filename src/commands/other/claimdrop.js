3fccb9ab01f0fc7febf3b49c8cc92d38bc8131c9
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-06-30 09:41:46 -0400

    attempt to fix multiple airdrop claims

--- src/commands/other/claimdrop.js
+++ src/commands/other/claimdrop.js
@@ -12,34 +12,36 @@ module.exports = {
     guildModsOnly: false,
     
     async execute(app, message){
-        const guildRow = await app.common.getGuildInfo(message.channel.guild.id);
-    
-        if(message.channel.id !== guildRow.dropItemChan){
-            return message.reply('There is no ' + app.itemdata['care_package'].icon + '`care_package` in this channel.');
-        }
-        
-        if(guildRow.dropItem === ''){
-            return message.reply('There is no lootable ' + app.itemdata['care_package'].icon + '`care_package`.');
-        }
-        
-        const airdropCD = await app.cd.getCD(message.author.id, 'airdrop');
+        setTimeout(async () => {
+            const guildRow = await app.common.getGuildInfo(message.channel.guild.id);
+            if(message.channel.id !== guildRow.dropItemChan || guildRow.dropItem === ''){
+                return message.reply('There is no ' + app.itemdata['care_package'].icon + '`care_package` in this channel.');
+            }
+            
+            const airdropCD = await app.cd.getCD(message.author.id, 'airdrop');
 
-        if(airdropCD){
-            return message.reply(`You need to wait \`${airdropCD}\` before claiming another airdrop.`);
-        }
-        
-        const itemCt = await app.itm.getItemCount(await app.itm.getItemObject(message.author.id), await app.player.getRow(message.author.id));
-        const hasEnough = await app.itm.hasSpace(itemCt, 1);
-        if(!hasEnough) return message.reply(`❌ **You don't have enough space in your inventory!** (You need **1** open slot, you have **${itemCt.open}**)\n\nYou can clear up space by selling some items.`);
-        
-        await app.query(`UPDATE guildInfo SET dropItem = '' WHERE guildId = ${message.channel.guild.id}`);
-        await app.query(`UPDATE guildInfo SET dropItemChan = 0 WHERE guildId = ${message.channel.guild.id}`);
+            if(airdropCD){
+                return message.reply(`You need to wait \`${airdropCD}\` before claiming another airdrop.`);
+            }
+            
+            const itemCt = await app.itm.getItemCount(await app.itm.getItemObject(message.author.id), await app.player.getRow(message.author.id));
+            const hasEnough = await app.itm.hasSpace(itemCt, 1);
+            if(!hasEnough) return message.reply(`❌ **You don't have enough space in your inventory!** (You need **1** open slot, you have **${itemCt.open}**)\n\nYou can clear up space by selling some items.`);
+            
+            const guildRow2 = await app.common.getGuildInfo(message.channel.guild.id);
+            if(message.channel.id !== guildRow2.dropItemChan || guildRow2.dropItem === ''){
+                return message.reply('There is no ' + app.itemdata['care_package'].icon + '`care_package` in this channel.');
+            }
 
-        if(await app.patreonHandler.isPatron(message.author.id, 4)) await app.cd.setCD(message.author.id, 'airdrop', Math.floor((app.config.cooldowns.claimdrop * 1000) / 2));
-        else await app.cd.setCD(message.author.id, 'airdrop', app.config.cooldowns.claimdrop * 1000);
+            await app.query(`UPDATE guildInfo SET dropItem = '' WHERE guildId = ${message.channel.guild.id}`);
+            await app.query(`UPDATE guildInfo SET dropItemChan = 0 WHERE guildId = ${message.channel.guild.id}`);
 
-        await app.itm.addItem(message.author.id, guildRow.dropItem, 1);
+            if(await app.patreonHandler.isPatron(message.author.id, 4)) await app.cd.setCD(message.author.id, 'airdrop', Math.floor((app.config.cooldowns.claimdrop * 1000) / 2));
+            else await app.cd.setCD(message.author.id, 'airdrop', app.config.cooldowns.claimdrop * 1000);
 
-        message.reply(`You got the ${app.itemdata[guildRow.dropItem].icon}\`${guildRow.dropItem}\`!`);
+            await app.itm.addItem(message.author.id, guildRow.dropItem, 1);
+
+            message.reply(`You got the ${app.itemdata[guildRow.dropItem].icon}\`${guildRow.dropItem}\`!`);
+        }, Math.floor(Math.random() * 1000) + 1);
     },
 }
