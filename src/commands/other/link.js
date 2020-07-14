9e4214cbb17f2775303a485bdebfe2a143e1f252
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-07-14 07:11:52 -0400

    replace discordapp.com with discord.com
    discordapp.com is being deprecated in November (except for the cdn)

--- src/commands/other/link.js
+++ src/commands/other/link.js
@@ -12,7 +12,7 @@ module.exports = {
     
     execute(app, message){
         const invite = new app.Embed()
-        .setDescription("You can invite Lootcord using this [link](https://discordapp.com/oauth2/authorize?client_id=493316754689359874&permissions=388160&scope=bot 'Click to invite Lootcord')!")
+        .setDescription("You can invite Lootcord using this [link](https://discord.com/oauth2/authorize?client_id=493316754689359874&permissions=388160&scope=bot 'Click to invite Lootcord')!")
         .setColor(13215302)
 
         message.channel.createMessage(invite);
