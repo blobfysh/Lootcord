9bcefec93f5c2dbd5fbc9cb31f949e2fe525f587
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-07-26 16:07:50 -0400

    add reroll_scroll, weekly gives reroll_scroll

--- src/commands/games/weekly.js
+++ src/commands/games/weekly.js
@@ -1,6 +1,6 @@
 const QUOTES = [
-    '✨ Oh look, I found this {icon}{item} for you!', 
-    '{ez} Here\'s a free {icon}{item}!'
+    '✨ Oh look, I found this {icon}{item} and {icon2}{item2} for you!', 
+    '{ez} Here\'s a free {icon}{item} and {icon2}{item2}!'
 ];
 
 module.exports = {
@@ -25,12 +25,19 @@ module.exports = {
         }
 
         const itemCt = await app.itm.getItemCount(await app.itm.getItemObject(message.author.id), await app.player.getRow(message.author.id));
-        const hasEnough = await app.itm.hasSpace(itemCt, 1);
-        if(!hasEnough) return message.reply(`❌ **You don't have enough space in your inventory!** (You need **1** open slot, you have **${itemCt.open}**)\n\nYou can clear up space by selling some items.`);
+        const hasEnough = await app.itm.hasSpace(itemCt, 2);
+        if(!hasEnough) return message.reply(`❌ **You don't have enough space in your inventory!** (You need **2** open slot, you have **${itemCt.open}**)\n\nYou can clear up space by selling some items.`);
 
         await app.cd.setCD(message.author.id, 'weekly', app.config.cooldowns.daily * 1000 * 7);
 
         await app.itm.addItem(message.author.id, 'supply_drop', 1);
-        message.reply(QUOTES[Math.floor(Math.random() * QUOTES.length)].replace('{ez}', app.icons.blackjack_dealer_neutral).replace('{icon}', app.itemdata['supply_drop'].icon).replace('{item}', '`supply_drop`'));
+        await app.itm.addItem(message.author.id, 'reroll_scroll', 1);
+
+        message.reply(QUOTES[Math.floor(Math.random() * QUOTES.length)]
+        .replace('{ez}', app.icons.blackjack_dealer_neutral)
+        .replace('{icon}', app.itemdata['supply_drop'].icon)
+        .replace('{item}', '`supply_drop`')
+        .replace('{icon2}', app.itemdata['reroll_scroll'].icon)
+        .replace('{item2}', '`reroll_scroll`'));
     },
 }
