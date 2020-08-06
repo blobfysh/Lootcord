7f8b3ca5da93ef320f24e8b01f1f7572f037306d
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-08-06 13:02:04 -0400

    "enemy" works for attacking spawns in channel

--- src/commands/items/use.js
+++ src/commands/items/use.js
@@ -229,7 +229,7 @@ module.exports = {
             }
             
             // check if attacking monster
-            if(Object.keys(app.mobdata).some(monster => message.args.map(arg => arg.toLowerCase()).includes('bounty') || message.args.map(arg => arg.toLowerCase()).includes('@bounty') || message.args.map(arg => arg.toLowerCase()).join(' ').includes(app.mobdata[monster].title.toLowerCase()))){
+            if(message.args.map(arg => arg.toLowerCase()).some(arg => ['@enemy', 'enemy', '@bounty', 'bounty'].includes(arg)) || Object.keys(app.mobdata).some(monster => message.args.map(arg => arg.toLowerCase()).join(' ').includes(app.mobdata[monster].title.toLowerCase()))){
                 const monsterRow = await app.mysql.select('spawns', 'channelId', message.channel.id);
 
                 if(!monsterRow){
