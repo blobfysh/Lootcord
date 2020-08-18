1ac622bcb0ede10b689c09ed59c369b2175e311f
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-08-18 08:38:00 -0400

    fix discoin limit

--- src/commands/other/convert.js
+++ src/commands/other/convert.js
@@ -51,7 +51,7 @@ module.exports = {
             // valid currency and user has money
             const response = await app.discoin.request(message.author.id, convertAmnt, currency);
             await app.player.removeMoney(message.author.id, convertAmnt);
-            await app.query("UPDATE scores SET discoinLimit = discoinLimit + ? WHERE userId = ?", [convertAmnt, message.author.id]);
+            //await app.query("UPDATE scores SET discoinLimit = discoinLimit + ? WHERE userId = ?", [convertAmnt, message.author.id]);
             
             const embed = new app.Embed()
             .setTitle('Successfully Converted!')
