4d082ce97c53170581c8170e35483199c78c0277
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-01-03 20:04:10 -0500

    new replies

--- src/utils/MessageUtils.js
+++ src/utils/MessageUtils.js
@@ -1,5 +1,20 @@
 const { Message } = require('eris')
 
 Message.prototype.reply = function(content) {
-	return this.channel.createMessage({ content: `<@${this.author.id}>, ${content}` })
+	if (typeof content === 'string') {
+		content = {
+			content
+		}
+	}
+
+	Object.assign(content, {
+		message_reference: {
+			message_id: this.id
+		},
+		allowedMentions: {
+			replied_user: true
+		}
+	})
+
+	return this.channel.createMessage(content)
 }
