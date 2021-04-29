53344cb9029cab47e5a82e58380eaf9f3318888a
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-04-29 17:10:32 -0400

    refactor profile command

--- src/commands/info/profile.js
+++ src/commands/info/profile.js
@@ -22,83 +22,95 @@ exports.command = {
 				return
 			}
 
-			userProfile(message.member)
+			message.reply(await makeProfile(app, message.author, serverSideGuildId))
 		}
 		else {
-			userProfile(memberArg)
-		}
-
-		async function userProfile(member) {
-			const userRow = await app.player.getRow(member.id, serverSideGuildId)
-
-			if (!userRow) {
-				return message.reply('❌ The person you\'re trying to search doesn\'t have an account!')
-			}
-
-			const userItems = await app.itm.getUserItems(await app.itm.getItemObject(member.id, serverSideGuildId), { onlyBanners: true })
-			const badges = await app.itm.getBadges(member.id, serverSideGuildId)
-			const xp = app.common.calculateXP(userRow.points, userRow.level)
-			const trivias = await app.player.getStat(message.author.id, 'trivias', serverSideGuildId)
-			const triviasCorrect = await app.player.getStat(message.author.id, 'triviasCorrect', serverSideGuildId)
-			const scrambles = await app.player.getStat(message.author.id, 'scrambles', serverSideGuildId)
-			const scramblesCorrect = await app.player.getStat(message.author.id, 'scramblesCorrect', serverSideGuildId)
-
-			const bannerIcon = app.itemdata[userRow.banner] !== undefined ? app.itemdata[userRow.banner].icon : ''
-			const bannersList = `**Equipped:** ${bannerIcon}\`${userRow.banner}\`\n${userItems.banners.join('\n')}`
-			let userStatus = 'Change your status with the `setstatus` command!'
-			let badgeList = ''
-			let healthStr = `**${userRow.health} / ${userRow.maxHealth}** HP${app.player.getHealthIcon(userRow.health, userRow.maxHealth, true)}`
-
-			if (userRow.bleed > 0) {
-				healthStr += `\n🩸 Bleeding: **${userRow.bleed}**`
-			}
-			if (userRow.burn > 0) {
-				healthStr += `\n🔥 Burning: **${userRow.burn}**`
-			}
-
-			if (userRow.status !== '') {
-				userStatus = userRow.status
-			}
-
-			if (badges.length) {
-				if (userRow.badge !== 'none') {
-					badgeList = `**Display**: ${app.badgedata[userRow.badge].icon}\`${userRow.badge}\`\n`
-				}
-
-				badgeList += badges.sort().filter(badge => badge !== userRow.badge).map(badge => `${app.badgedata[badge].icon}\`${badge}\``).join('\n')
-			}
-			else {
-				badgeList = 'None'
-			}
-
-			const profileEmbed = new app.Embed()
-				.setColor(13451564)
-				.setAuthor(`${member.username}#${member.discriminator}'s Profile`, member.avatarURL)
-				.setDescription(userStatus)
-				.addField('Clan', codeWrap(userRow.clanId !== 0 ? (await app.clans.getRow(userRow.clanId)).name : 'None', 'js'), true)
-				.addField('Level', codeWrap(`${userRow.level} (${xp.curLvlXp} / ${xp.neededForLvl})`, 'js'), true)
-				.addField('Power', codeWrap(`${userRow.power} / ${userRow.max_power} Power`, 'js'), true)
-				.addField('K/D Ratio', userRow.deaths === 0 ? `${userRow.kills} Kills\n${userRow.deaths} Deaths (${userRow.kills} K/D)\n` : `${userRow.kills} Kills\n${userRow.deaths} Deaths (${(userRow.kills / userRow.deaths).toFixed(2)} K/D)`, true)
-				.addField('Trivia Stats', `Attempts: ${trivias}\nCorrect: ${triviasCorrect}`, true)
-				.addField('Scramble Stats', `Attempts: ${scrambles}\nCorrect: ${scramblesCorrect}`, true)
-				.addField('Health', healthStr, true)
-				.addField('Strength', `${parseFloat(userRow.scaledDamage).toFixed(2)}x damage`, true)
-				.addField('Luck', userRow.luck.toString(), true)
-				.addField('Banners', bannersList, true)
-				.addField('Badges', badgeList, true)
-				.addField('Preferred Ammo', app.itemdata[userRow.ammo] ? `${app.itemdata[userRow.ammo].icon}\`${userRow.ammo}\`` : 'Not set', true)
-				.setFooter(`🌟 Skills upgraded ${userRow.used_stats} times`)
-
-			if (userRow.banner !== 'none') {
-				profileEmbed.setImage(app.itemdata[userRow.banner].image)
-				profileEmbed.setColor(app.itemdata[userRow.banner].bannerColor)
-			}
-
-			message.channel.createMessage(profileEmbed)
+			message.reply(await makeProfile(app, memberArg, serverSideGuildId))
 		}
 	}
 }
 
 function codeWrap(input, code) {
 	return `\`\`\`${code}\n${input}\`\`\``
+}
+
+const makeProfile = exports.makeProfile = async function makeProfile(app, user, serverSideGuildId) {
+	try {
+		const userRow = await app.player.getRow(user.id, serverSideGuildId)
+
+		if (!userRow) {
+			return {
+				content: '❌ The person you\'re trying to search doesn\'t have an account!'
+			}
+		}
+
+		const userItems = await app.itm.getUserItems(await app.itm.getItemObject(user.id, serverSideGuildId), { onlyBanners: true })
+		const badges = await app.itm.getBadges(user.id, serverSideGuildId)
+		const xp = app.common.calculateXP(userRow.points, userRow.level)
+		const trivias = await app.player.getStat(user.id, 'trivias', serverSideGuildId)
+		const triviasCorrect = await app.player.getStat(user.id, 'triviasCorrect', serverSideGuildId)
+		const scrambles = await app.player.getStat(user.id, 'scrambles', serverSideGuildId)
+		const scramblesCorrect = await app.player.getStat(user.id, 'scramblesCorrect', serverSideGuildId)
+
+		const bannerIcon = app.itemdata[userRow.banner] !== undefined ? app.itemdata[userRow.banner].icon : ''
+		const bannersList = `**Equipped:** ${bannerIcon}\`${userRow.banner}\`\n${userItems.banners.join('\n')}`
+		let userStatus = 'Change your status with the `setstatus` command!'
+		let badgeList = ''
+		let healthStr = `**${userRow.health} / ${userRow.maxHealth}** HP${app.player.getHealthIcon(userRow.health, userRow.maxHealth, true)}`
+
+		if (userRow.bleed > 0) {
+			healthStr += `\n🩸 Bleeding: **${userRow.bleed}**`
+		}
+		if (userRow.burn > 0) {
+			healthStr += `\n🔥 Burning: **${userRow.burn}**`
+		}
+
+		if (userRow.status !== '') {
+			userStatus = userRow.status
+		}
+
+		if (badges.length) {
+			if (userRow.badge !== 'none') {
+				badgeList = `**Display**: ${app.badgedata[userRow.badge].icon}\`${userRow.badge}\`\n`
+			}
+
+			badgeList += badges.sort().filter(badge => badge !== userRow.badge).map(badge => `${app.badgedata[badge].icon}\`${badge}\``).join('\n')
+		}
+		else {
+			badgeList = 'None'
+		}
+
+		const profileEmbed = new app.Embed()
+			.setColor(13451564)
+			.setAuthor(`${user.username}#${user.discriminator}'s Profile`, app.common.getAvatar(user))
+			.setDescription(userStatus)
+			.addField('Clan', codeWrap(userRow.clanId !== 0 ? (await app.clans.getRow(userRow.clanId)).name : 'None', 'js'), true)
+			.addField('Level', codeWrap(`${userRow.level} (${xp.curLvlXp} / ${xp.neededForLvl})`, 'js'), true)
+			.addField('Power', codeWrap(`${userRow.power} / ${userRow.max_power} Power`, 'js'), true)
+			.addField('K/D Ratio', userRow.deaths === 0 ? `${userRow.kills} Kills\n${userRow.deaths} Deaths (${userRow.kills} K/D)\n` : `${userRow.kills} Kills\n${userRow.deaths} Deaths (${(userRow.kills / userRow.deaths).toFixed(2)} K/D)`, true)
+			.addField('Trivia Stats', `Attempts: ${trivias}\nCorrect: ${triviasCorrect}`, true)
+			.addField('Scramble Stats', `Attempts: ${scrambles}\nCorrect: ${scramblesCorrect}`, true)
+			.addField('Health', healthStr, true)
+			.addField('Strength', `${parseFloat(userRow.scaledDamage).toFixed(2)}x damage`, true)
+			.addField('Luck', userRow.luck.toString(), true)
+			.addField('Banners', bannersList, true)
+			.addField('Badges', badgeList, true)
+			.addField('Preferred Ammo', app.itemdata[userRow.ammo] ? `${app.itemdata[userRow.ammo].icon}\`${userRow.ammo}\`` : 'Not set', true)
+			.setFooter(`🌟 Skills upgraded ${userRow.used_stats} times`)
+
+		if (userRow.banner !== 'none') {
+			profileEmbed.setImage(app.itemdata[userRow.banner].image)
+			profileEmbed.setColor(app.itemdata[userRow.banner].bannerColor)
+		}
+
+		return {
+			embed: profileEmbed.embed
+		}
+	}
+	catch (err) {
+		console.log(err)
+		return {
+			content: '❌ There was an error trying to fetch profile. Make sure you mention the user.'
+		}
+	}
 }
--- src/commands/moderation/getprofile.js
+++ src/commands/moderation/getprofile.js
@@ -1,3 +1,5 @@
+const { makeProfile } = require('../info/profile')
+
 exports.command = {
 	name: 'getprofile',
 	aliases: ['getp'],
@@ -20,81 +22,9 @@ exports.command = {
 			return message.reply('❌ You forgot to include a user ID.')
 		}
 
-		try {
-			const row = await app.player.getRow(userID.toString())
+		const userInfo = await app.common.fetchUser(userID, { cacheIPC: false })
+		const profile = await makeProfile(app, userInfo, undefined)
 
-			if (!row) {
-				return message.reply('❌ User has no account.')
-			}
-
-			const userInfo = await app.common.fetchUser(userID, { cacheIPC: false })
-			const userItems = await app.itm.getUserItems(await app.itm.getItemObject(userID), { onlyBanners: true })
-			const badges = await app.itm.getBadges(userID)
-			const xp = app.common.calculateXP(row.points, row.level)
-			const trivias = await app.player.getStat(message.author.id, 'trivias')
-			const triviasCorrect = await app.player.getStat(message.author.id, 'triviasCorrect')
-			const scrambles = await app.player.getStat(message.author.id, 'scrambles')
-			const scramblesCorrect = await app.player.getStat(message.author.id, 'scramblesCorrect')
-
-			const bannerIcon = app.itemdata[row.banner] !== undefined ? app.itemdata[row.banner].icon : ''
-			const bannersList = `**Equipped:** ${bannerIcon}\`${row.banner}\`\n${userItems.banners.join('\n')}`
-			let userStatus = 'Change your status with the `setstatus` command!'
-			let badgeList = ''
-			let healthStr = `**${row.health} / ${row.maxHealth}** HP${app.player.getHealthIcon(row.health, row.maxHealth, true)}`
-
-			if (row.bleed > 0) {
-				healthStr += `\n🩸 Bleeding: **${row.bleed}**`
-			}
-			if (row.burn > 0) {
-				healthStr += `\n🔥 Burning: **${row.burn}**`
-			}
-
-			if (row.status !== '') {
-				userStatus = row.status
-			}
-
-			if (badges.length) {
-				if (row.badge !== 'none') {
-					badgeList = `**Display**: ${app.badgedata[row.badge].icon}\`${row.badge}\`\n`
-				}
-
-				badgeList += badges.sort().filter(badge => badge !== row.badge).map(badge => `${app.badgedata[badge].icon}\`${badge}\``).join('\n')
-			}
-			else {
-				badgeList = 'None'
-			}
-
-			const profileEmbed = new app.Embed()
-				.setColor(13451564)
-				.setAuthor(`${userInfo.username}#${userInfo.discriminator}'s Profile`, app.common.getAvatar(userInfo))
-				.setDescription(userStatus)
-				.addField('Clan', codeWrap(row.clanId !== 0 ? (await app.clans.getRow(row.clanId)).name : 'None', 'js'), true)
-				.addField('Level', codeWrap(`${row.level} (${xp.curLvlXp} / ${xp.neededForLvl})`, 'js'), true)
-				.addField('Power', codeWrap(`${row.power} / ${row.max_power} Power`, 'js'), true)
-				.addField('K/D Ratio', row.deaths === 0 ? `${row.kills} Kills\n${row.deaths} Deaths (${row.kills} K/D)\n` : `${row.kills} Kills\n${row.deaths} Deaths (${(row.kills / row.deaths).toFixed(2)} K/D)`, true)
-				.addField('Trivia Stats', `Attempts: ${trivias}\nCorrect: ${triviasCorrect}`, true)
-				.addField('Scramble Stats', `Attempts: ${scrambles}\nCorrect: ${scramblesCorrect}`, true)
-				.addField('Health', healthStr, true)
-				.addField('Strength', `${parseFloat(row.scaledDamage).toFixed(2)}x damage`, true)
-				.addField('Luck', row.luck.toString(), true)
-				.addField('Banners', bannersList, true)
-				.addField('Badges', badgeList, true)
-				.addField('Preferred Ammo', app.itemdata[row.ammo] ? `${app.itemdata[row.ammo].icon}\`${row.ammo}\`` : 'Not set', true)
-				.setFooter(`🌟 Skills upgraded ${row.used_stats} times`)
-
-			if (row.banner !== 'none') {
-				profileEmbed.setImage(app.itemdata[row.banner].image)
-				profileEmbed.setColor(app.itemdata[row.banner].bannerColor)
-			}
-
-			message.channel.createMessage(profileEmbed)
-		}
-		catch (err) {
-			message.reply(`Error:\`\`\`${err}\`\`\``)
-		}
+		message.reply(profile)
 	}
-}
-
-function codeWrap(input, code) {
-	return `\`\`\`${code}\n${input}\`\`\``
 }
