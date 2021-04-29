a99988a8e5ca0a56fbdcd239958f049285952e00
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-04-29 16:13:36 -0400

    refector inventory command

--- src/commands/info/inventory.js
+++ src/commands/info/inventory.js
@@ -15,7 +15,7 @@ exports.command = {
 	requiresActive: false,
 	guildModsOnly: false,
 
-	execute(app, message, { args, prefix, guildInfo, serverSideGuildId }) {
+	async execute(app, message, { args, prefix, guildInfo, serverSideGuildId }) {
 		const memberArg = app.parse.members(message, args)[0]
 
 		// no member found in ArgParser
@@ -26,98 +26,105 @@ exports.command = {
 				return
 			}
 
-			makeInventory(message.member)
+			message.reply(await makeInventory(app, message.author, message.channel.guild.id, serverSideGuildId))
 		}
 		else {
-			makeInventory(memberArg)
+			message.reply(await makeInventory(app, memberArg, message.channel.guild.id, serverSideGuildId))
+		}
+	}
+}
+
+
+const makeInventory = exports.makeInventory = async function makeInventory(app, user, guildId, serverSideGuildId) {
+	try {
+		const userRow = await app.player.getRow(user.id, serverSideGuildId)
+
+		if (!userRow) {
+			return {
+				content: '❌ The person you\'re trying to search doesn\'t have an account!'
+			}
 		}
 
-		async function makeInventory(member) {
-			try {
-				const userRow = await app.player.getRow(member.id, serverSideGuildId)
+		const isActive = guildId ? await app.player.isActive(user.id, guildId) : false
+		const itemObject = await app.itm.getItemObject(user.id, serverSideGuildId)
+		const usersItems = await app.itm.getUserItems(itemObject)
+		const itemCt = await app.itm.getItemCount(itemObject, userRow)
+		const armorLeft = await app.cd.getCD(user.id, 'shield', { serverSideGuildId })
+		const armor = await app.player.getArmor(user.id, serverSideGuildId)
+		const passiveShield = await app.cd.getCD(user.id, 'passive_shield', { serverSideGuildId })
 
-				if (!userRow) {
-					return message.reply('❌ The person you\'re trying to search doesn\'t have an account!')
-				}
+		const embedInfo = new app.Embed()
+			.setTitle(`${isActive ? app.icons.accounts.active : app.icons.accounts.inactive} ${`${user.username}#${user.discriminator}`}'s Inventory`)
+			.setColor(13451564)
 
-				const isActive = await app.player.isActive(member.id, member.guild.id)
-				const itemObject = await app.itm.getItemObject(member.id, serverSideGuildId)
-				const usersItems = await app.itm.getUserItems(itemObject)
-				const itemCt = await app.itm.getItemCount(itemObject, userRow)
-				const armorLeft = await app.cd.getCD(member.id, 'shield', { serverSideGuildId })
-				const armor = await app.player.getArmor(member.id, serverSideGuildId)
-				const passiveShield = await app.cd.getCD(member.id, 'passive_shield', { serverSideGuildId })
+		if (armorLeft) {
+			embedInfo.addField(armor ? 'Armor' : '🛡️ Armor', armor ? `${app.itemdata[armor].icon}\`${armor}\` (\`${armorLeft}\`)` : `\`${armorLeft}\``)
+		}
+		if (passiveShield) {
+			embedInfo.addField('🛡️ Passive Shield', `\`${passiveShield}\` [?](https://lootcord.com/faq#what-is-a-passive-shield 'A passive shield is a 24 hour attack shield given to you when you are killed.\n\nThis shield will automatically be removed if you decide to attack someone.')`)
+		}
 
-				const embedInfo = new app.Embed()
-					.setTitle(`${isActive ? app.icons.accounts.active : app.icons.accounts.inactive} ${`${member.username}#${member.discriminator}`}'s Inventory`)
-					.setColor(13451564)
+		let healthStr = `**${userRow.health} / ${userRow.maxHealth}** HP${app.player.getHealthIcon(userRow.health, userRow.maxHealth, true)}`
 
-				if (armorLeft) {
-					embedInfo.addField(armor ? 'Armor' : '🛡️ Armor', armor ? `${app.itemdata[armor].icon}\`${armor}\` (\`${armorLeft}\`)` : `\`${armorLeft}\``)
-				}
-				if (passiveShield) {
-					embedInfo.addField('🛡️ Passive Shield', `\`${passiveShield}\` [?](https://lootcord.com/faq#what-is-a-passive-shield 'A passive shield is a 24 hour attack shield given to you when you are killed.\n\nThis shield will automatically be removed if you decide to attack someone.')`)
-				}
+		if (userRow.bleed > 0) {
+			healthStr += `\n🩸 Bleeding: **${userRow.bleed}**`
+		}
+		if (userRow.burn > 0) {
+			healthStr += `\n🔥 Burning: **${userRow.burn}**`
+		}
 
-				let healthStr = `**${userRow.health} / ${userRow.maxHealth}** HP${app.player.getHealthIcon(userRow.health, userRow.maxHealth, true)}`
+		embedInfo.addField('Health', healthStr, true)
 
-				if (userRow.bleed > 0) {
-					healthStr += `\n🩸 Bleeding: **${userRow.bleed}**`
-				}
-				if (userRow.burn > 0) {
-					healthStr += `\n🔥 Burning: **${userRow.burn}**`
-				}
+		embedInfo.addField('Money', `${app.common.formatNumber(userRow.money)}\n${app.common.formatNumber(userRow.scrap, false, true)}`, true)
 
-				embedInfo.addField('Health', healthStr, true)
+		if (userRow.backpack === 'none') {
+			embedInfo.addField('Storage Container', 'None equipped', true)
+		}
+		else {
+			embedInfo.addField('Storage Container', `${app.itemdata[userRow.backpack].icon}\`${userRow.backpack}\``, true)
+		}
 
-				embedInfo.addField('Money', `${app.common.formatNumber(userRow.money)}\n${app.common.formatNumber(userRow.scrap, false, true)}`, true)
+		embedInfo.addBlankField()
 
-				if (userRow.backpack === 'none') {
-					embedInfo.addField('Storage Container', 'None equipped', true)
-				}
-				else {
-					embedInfo.addField('Storage Container', `${app.itemdata[userRow.backpack].icon}\`${userRow.backpack}\``, true)
-				}
+		// item fields
+		if (usersItems.ranged.length) {
+			embedInfo.addField(ITEM_TYPES.ranged.name, usersItems.ranged.join('\n'), true)
+		}
 
-				embedInfo.addBlankField()
+		if (usersItems.melee.length) {
+			embedInfo.addField(ITEM_TYPES.melee.name, usersItems.melee.join('\n'), true)
+		}
 
-				// item fields
-				if (usersItems.ranged.length) {
-					embedInfo.addField(ITEM_TYPES.ranged.name, usersItems.ranged.join('\n'), true)
-				}
+		if (usersItems.usables.length) {
+			embedInfo.addField(ITEM_TYPES.items.name, usersItems.usables.join('\n'), true)
+		}
 
-				if (usersItems.melee.length) {
-					embedInfo.addField(ITEM_TYPES.melee.name, usersItems.melee.join('\n'), true)
-				}
+		if (usersItems.ammo.length) {
+			embedInfo.addField(ITEM_TYPES.ammo.name, usersItems.ammo.join('\n'), true)
+		}
 
-				if (usersItems.usables.length) {
-					embedInfo.addField(ITEM_TYPES.items.name, usersItems.usables.join('\n'), true)
-				}
+		if (usersItems.materials.length) {
+			embedInfo.addField(ITEM_TYPES.materials.name, usersItems.materials.join('\n'), true)
+		}
 
-				if (usersItems.ammo.length) {
-					embedInfo.addField(ITEM_TYPES.ammo.name, usersItems.ammo.join('\n'), true)
-				}
+		if (usersItems.storage.length) {
+			embedInfo.addField(ITEM_TYPES.storage.name, usersItems.storage.join('\n'), true)
+		}
 
-				if (usersItems.materials.length) {
-					embedInfo.addField(ITEM_TYPES.materials.name, usersItems.materials.join('\n'), true)
-				}
+		if (!usersItems.ranged.length && !usersItems.melee.length && !usersItems.usables.length && !usersItems.ammo.length && !usersItems.materials.length && !usersItems.storage.length) {
+			embedInfo.addField('This inventory is empty! :(', '\u200b')
+		}
 
-				if (usersItems.storage.length) {
-					embedInfo.addField(ITEM_TYPES.storage.name, usersItems.storage.join('\n'), true)
-				}
+		embedInfo.addField('\u200b', `Inventory space: ${itemCt.capacity} max | Value: ${app.common.formatNumber(usersItems.invValue + userRow.money)}`)
 
-				if (!usersItems.ranged.length && !usersItems.melee.length && !usersItems.usables.length && !usersItems.ammo.length && !usersItems.materials.length && !usersItems.storage.length) {
-					embedInfo.addField('This inventory is empty! :(', '\u200b')
-				}
-
-				embedInfo.addField('\u200b', `Inventory space: ${itemCt.capacity} max | Value: ${app.common.formatNumber(usersItems.invValue + userRow.money)}`)
-
-				await message.channel.createMessage(embedInfo)
-			}
-			catch (err) {
-				console.log(err)
-				message.reply('❌ There was an error trying to fetch inventory. Make sure you mention the user.')
-			}
+		return {
+			embed: embedInfo.embed
+		}
+	}
+	catch (err) {
+		console.log(err)
+		return {
+			content: '❌ There was an error trying to fetch inventory. Make sure you mention the user.'
 		}
 	}
 }
--- src/commands/moderation/getinv.js
+++ src/commands/moderation/getinv.js
@@ -1,4 +1,4 @@
-const { ITEM_TYPES } = require('../../resources/constants')
+const { makeInventory } = require('../info/inventory')
 
 exports.command = {
 	name: 'getinv',
@@ -22,89 +22,9 @@ exports.command = {
 			return message.reply('❌ You forgot to include a user ID.')
 		}
 
-		try {
-			const row = await app.player.getRow(userID)
+		const userInfo = await app.common.fetchUser(userID, { cacheIPC: false })
+		const inv = await makeInventory(app, userInfo, message.channel.guild.id, undefined)
 
-			if (!row) {
-				return message.reply('❌ User has no account.')
-			}
-
-			const userInfo = await app.common.fetchUser(userID, { cacheIPC: false })
-			const itemObject = await app.itm.getItemObject(userID)
-			const usersItems = await app.itm.getUserItems(itemObject)
-			const itemCt = await app.itm.getItemCount(itemObject, row)
-			const armorLeft = await app.cd.getCD(userID, 'shield')
-			const armor = await app.player.getArmor(userID)
-			const passiveShield = await app.cd.getCD(userID, 'passive_shield')
-
-			const embedInfo = new app.Embed()
-				.setTitle(`${userInfo.username}#${userInfo.discriminator}'s Inventory`)
-				.setColor(13451564)
-
-			if (armorLeft) {
-				embedInfo.addField(armor ? 'Armor' : '🛡️ Armor', armor ? `${app.itemdata[armor].icon}\`${armorLeft}\`` : `\`${armorLeft}\``)
-			}
-			if (passiveShield) {
-				embedInfo.addField('🛡️ Passive Shield', `\`${passiveShield}\``)
-			}
-
-			let healthStr = `**${row.health} / ${row.maxHealth}** HP${app.player.getHealthIcon(row.health, row.maxHealth, true)}`
-
-			if (row.bleed > 0) {
-				healthStr += `\n🩸 Bleeding: **${row.bleed}**`
-			}
-			if (row.burn > 0) {
-				healthStr += `\n🔥 Burning: **${row.burn}**`
-			}
-
-			embedInfo.addField('Health', healthStr, true)
-
-			embedInfo.addField('Money', `${app.common.formatNumber(row.money)}\n${app.common.formatNumber(row.scrap, false, true)}`, true)
-
-			if (row.backpack === 'none') {
-				embedInfo.addField('Storage Container', 'None equipped', true)
-			}
-			else {
-				embedInfo.addField('Storage Container', `${app.itemdata[row.backpack].icon}\`${row.backpack}\``, true)
-			}
-
-			embedInfo.addBlankField()
-
-			// item fields
-			if (usersItems.ranged.length) {
-				embedInfo.addField(ITEM_TYPES.ranged.name, usersItems.ranged.join('\n'), true)
-			}
-
-			if (usersItems.melee.length) {
-				embedInfo.addField(ITEM_TYPES.melee.name, usersItems.melee.join('\n'), true)
-			}
-
-			if (usersItems.usables.length) {
-				embedInfo.addField(ITEM_TYPES.items.name, usersItems.usables.join('\n'), true)
-			}
-
-			if (usersItems.ammo.length) {
-				embedInfo.addField(ITEM_TYPES.ammo.name, usersItems.ammo.join('\n'), true)
-			}
-
-			if (usersItems.materials.length) {
-				embedInfo.addField(ITEM_TYPES.materials.name, usersItems.materials.join('\n'), true)
-			}
-
-			if (usersItems.storage.length) {
-				embedInfo.addField(ITEM_TYPES.storage.name, usersItems.storage.join('\n'), true)
-			}
-
-			if (!usersItems.ranged.length && !usersItems.melee.length && !usersItems.usables.length && !usersItems.ammo.length && !usersItems.materials.length && !usersItems.storage.length) {
-				embedInfo.addField('This inventory is empty! :(', '\u200b')
-			}
-
-			embedInfo.addField('\u200b', `Inventory space: ${itemCt.capacity} max | Value: ${app.common.formatNumber(usersItems.invValue)}`)
-
-			message.channel.createMessage(embedInfo)
-		}
-		catch (err) {
-			message.reply(`Error:\`\`\`${err}\`\`\``)
-		}
+		message.reply(inv)
 	}
 }
