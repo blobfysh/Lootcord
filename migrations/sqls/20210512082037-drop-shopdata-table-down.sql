2087a16c2c1f1d15dd2d2770316d0149b918430b
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-05-12 04:24:37 -0400

    remove old shop data

---
+++ migrations/sqls/20210512082037-drop-shopdata-table-down.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS shopdata (
+	itemName VARCHAR(255),
+	itemAmount INT,
+	itemPrice INT,
+	itemCurrency VARCHAR(255),
+	itemDisplay VARCHAR(255),
+	item VARCHAR(255)
+) ENGINE = InnoDB;
