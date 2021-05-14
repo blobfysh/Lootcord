d2aa862c28b00451aa09b7164a725e03ef84bc55
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-05-14 00:22:31 -0400

    add beg command

---
+++ src/commands/rewards/beg.js
@@ -0,0 +1,33 @@
+exports.command = {
+	name: 'beg',
+	aliases: [],
+	description: 'Beg for some scrap.',
+	long: 'Are you broke? Lose all your scrap from gambling? Only one solution: go bug people for some scrap.',
+	args: {},
+	examples: [],
+	permissions: ['sendMessages', 'externalEmojis'],
+	ignoreHelp: false,
+	requiresAcc: true,
+	requiresActive: true,
+	guildModsOnly: false,
+
+	async execute(app, message, { args, prefix, guildInfo, serverSideGuildId }) {
+		const row = await app.player.getRow(message.author.id, serverSideGuildId)
+		const { itemCt } = await app.itm.getItemCount(await app.itm.getItemObject(message.author.id, serverSideGuildId), row)
+		if (row.money >= 500) {
+			return message.reply(`${app.icons.bruhface} Bruh, you have **${app.common.formatNumber(row.money)}** scrap. You don't need to beg.`)
+		}
+		else if (itemCt >= 2) {
+			return message.reply(`${app.icons.banditguard} What?? You have **${itemCt}** perfectly good items, just sell them if you want scrap.`)
+		}
+		else if (row.clanId !== 0) {
+			return message.reply(`${app.icons.banditguard} Aren't you in a clan? Why don't you go beg them for stuff >:(`)
+		}
+
+		console.log(`${message.author.username} (${message.author.id}) is begging for scrap`)
+		const randAmt = Math.floor((Math.random() * (1000 - 500 + 1)) + 500)
+		await message.reply(`${app.icons.banditguard} dang ur broke lol. Take this **${app.common.formatNumber(randAmt)}** scrap and get outta my face.`)
+
+		await app.player.addMoney(message.author.id, randAmt, serverSideGuildId)
+	}
+}
