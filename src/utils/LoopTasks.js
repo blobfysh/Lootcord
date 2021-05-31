ab78435f7ca4a242ca772977bf48a1eff5eae9cc
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-05-31 00:06:25 -0400

    fix auto-deactivation bug
    it was deactivating users who were still active because they were inactive in a separate server

--- src/utils/LoopTasks.js
+++ src/utils/LoopTasks.js
@@ -97,7 +97,7 @@ class LoopTasks {
 			ON userguilds.userId = server_scores.userId
 			INNER JOIN guildinfo
 			ON userguilds.guildId = guildinfo.guildId
-			WHERE server_scores.lastActive < NOW() - INTERVAL 7 DAY AND serverOnly = 1`)
+			WHERE server_scores.lastActive < NOW() - INTERVAL 7 DAY AND serverOnly = 1 AND server_scores.guildId = guildinfo.guildId`)
 		let activeRolesRemoved = 0
 
 		for (const inactiveUser of globalInactiveUsers) {
