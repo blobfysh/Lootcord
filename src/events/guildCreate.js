4c9e75b923e43b4ac49479116bfd597a815a300e
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-06-16 20:23:58 -0400

    log guilds joined/left

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -2,4 +2,12 @@ exports.run = async function (guild) {
 	await this.cache.incr('servers_joined')
 
 	if (await this.cd.getCD(guild.id, 'guildbanned')) guild.leave()
+
+	const guildEmbed = new this.Embed()
+		.setTitle('Joined Server')
+		.setDescription(`**Name**: ${guild.name}\n` +
+			`**ID**: ${guild.id}`)
+		.setColor(9043800)
+
+	this.messager.messageLogs(guildEmbed)
 }
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -6,4 +6,13 @@ exports.run = async function (guild) {
 	await this.query(`DELETE FROM guildprefix WHERE guildId ="${guild.id}"`)
 	await this.cache.del(`prefix|${guild.id}`)
 	await this.query(`DELETE FROM guildinfo WHERE guildId ="${guild.id}"`)
+
+	const guildEmbed = new this.Embed()
+		.setTitle('Left Server')
+		.setDescription(`**Name**: ${guild.name}\n` +
+			`**ID**: ${guild.id}\n` +
+			`**Joined**: ${this.common.getShortDate(guild.joinedAt)}`)
+		.setColor(16734296)
+
+	this.messager.messageLogs(guildEmbed)
 }
