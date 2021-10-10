12de1155c690b3f929c277d25c5073dd33c1a027
blobfysh <47480729+blobfysh@users.noreply.github.com>
2021-10-09 21:53:10 -0400

    add halloween events

---
+++ src/handlers/events/trickortreatrare.js
@@ -0,0 +1,61 @@
+module.exports = {
+	name: 'trickortreatrare',
+	cooldown: 3600 * 1000,
+
+	async execute (app, message, { prefix, serverSideGuildId, eventPingRole }) {
+		console.log('[EVENT] Halloween event started')
+
+		const collectorObj = app.msgCollector.createChannelCollector(message.channel.id, m => m.content.toLowerCase() === 'steal bag', { time: 40000 })
+
+		const exploreEmbed = new app.Embed()
+			.setColor('#881EE4')
+			.setTitle('Event - __CANDY THEFT__')
+			.setDescription(`**Someone dropped their ${app.itemdata.medium_loot_bag.icon}\`medium_loot_bag\`!**\n\nQuick, type \`steal bag\` to grab it!`)
+			.setImage(app.itemdata.medium_loot_bag.image)
+
+		try {
+			const startedMessage = await message.channel.createMessage({
+				content: eventPingRole ? `<@&${eventPingRole}>` : undefined,
+				embed: exploreEmbed.embed
+			})
+			const joined = {}
+
+			collectorObj.collector.on('collect', async m => {
+				if (!await app.player.isActive(m.author.id, m.channel.guild.id)) return m.channel.createMessage(`Your account is not active in this server! Use \`${prefix}play\` to activate it here`)
+
+				// ignore users who have already joined this event
+				else if (Object.keys(joined).includes(m.author.id)) return
+
+				// max 20 people per event
+				else if (Object.keys(joined).length >= 20) return
+
+				joined[m.author.id] = m.author
+				m.addReaction(app.icons.confirm)
+			})
+
+			collectorObj.collector.on('end', async reason => {
+				exploreEmbed.setDescription(`~~**Someone dropped their ${app.itemdata.medium_loot_bag.icon}\`medium_loot_bag\`!**\n\nQuick, type \`steal bag\` to grab it!~~\n❌ This event has ended and is no longer accepting responses! ${app.icons.blackjack_dealer_lost}`)
+				startedMessage.edit(exploreEmbed)
+
+				const participants = Object.keys(joined)
+
+				if (participants.length) {
+					const winner = participants[Math.floor(Math.random() * participants.length)]
+
+					await app.itm.addItem(winner, 'medium_loot_bag', 1, serverSideGuildId)
+
+					const resultsEmb = new app.Embed()
+						.setColor('#881EE4')
+						.setTitle('Event Results - __CANDY THEFT__')
+						.setDescription(`<@${winner}> stole the ${app.itemdata.medium_loot_bag.icon}\`medium_loot_bag\`!`)
+
+					await message.channel.createMessage(resultsEmb)
+				}
+			})
+		}
+		catch (err) {
+			// kicked bot during event?
+			console.warn(err)
+		}
+	}
+}
